1a0f14c6555eafa2200ed699608acb7a6f0b2e73
Yashika Jain <jyashika717@gmail.com>
2022-07-08 08:00:28 +0530

    Add custom middleware

---
+++ WebApplication1/WebApplication1/Views/About/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Home1
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
