1f8f702a280b9b305f234cb0a17739f2e9ee188a
Yashika Jain <48827197+yj2303@users.noreply.github.com>
2022-07-11 10:50:27 -0700

    Add assignment-1

---
+++ assignments/assignment1/ConsoleApp3/CodeFile1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleAppConditionalLinq
+{
+    public static class LINQExtensions
+    {
+        public static IQueryable<T> If<T>(
+            this IQueryable<T> query,
+            bool should,
+            params Func<IQueryable<T>, IQueryable<T>>[] transforms)
+        {
+            return should
+                ? transforms.Aggregate(query,
+                    (current, transform) => transform.Invoke(current))
+                : query;
+        }
+
+        public static IEnumerable<T> If<T>(
+            this IEnumerable<T> query,
+            bool should,
+            params Func<IEnumerable<T>, IEnumerable<T>>[] transforms)
+        {
+            return should
+                ? transforms.Aggregate(query,
+                    (current, transform) => transform.Invoke(current))
+                : query;
+        }
+    }
+
+    class DataModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool SexIsFemale { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public int SocialSecurityNr { get; set; }
+    }
+
+    class Program
+    {
+        // data to be searched
+        static List<string> theData;
+
+        // search for this&that Y/N
+        static bool searchForFirstName = false;
+        static bool searchForLastName = false;
+        static bool searchForOnlyFemale = true;
+        static bool searchForYearOfBirthGreaterThan = true;
+        static bool searchForSocialSecurityGreaterThan = true;
+
+        // search"terms"
+        static string searchTermFirstName = "Fox";
+        static string searchTermLastName = "Mulder";
+        static int searchTermYearOfBirth = 1980;
+        static int searchTermSocialSecNr = 420000;
+
+        static void Main(string[] args)
+        {
+            PopulateAndFilterData();
+            DisplayData(theData);
+
+            Console.WriteLine("");
+            Console.WriteLine("now search with searchForOnlyFemale set to FALSE:");
+            Console.WriteLine("");
+
+            // now don't only search exclusive for women
+            searchForOnlyFemale = false;
+            PopulateAndFilterData();
+            DisplayData(theData);
+
+            Console.WriteLine("");
+            Console.WriteLine("now search for LastName='Smith'");
+            Console.WriteLine("");
+
+            // now only search for lastname like('Smith') and soc.sec.nr > 300000
+            searchForFirstName = false;
+            searchForLastName = true;
+            searchForOnlyFemale = false;
+            searchForYearOfBirthGreaterThan = false;
+            searchForSocialSecurityGreaterThan = true;
+
+            searchTermLastName = "Smith";
+            searchTermSocialSecNr = 300000;
+
+            PopulateAndFilterData();
+            DisplayData(theData);
+
+            Console.ReadKey();
+        }
+
+        static void PopulateAndFilterData()
+        {
+            theData = PopulateData();
+            theData = theData.If(searchForFirstName,
+                            q => q.Where(_ => _.FirstName == searchTermFirstName))
+                      .If(searchForLastName,
+                            q => q.Where(_ => _.LastName == searchTermLastName))
+                      .If(searchForOnlyFemale,
+                            q => q.Where(_ => _.SexIsFemale))
+                      .If(searchForYearOfBirthGreaterThan,
+                            q => q.Where(_ => _.DayOfBirth.Year > searchTermYearOfBirth))
+                      .If(searchForSocialSecurityGreaterThan,
+                            q => q.Where(_ => _.SocialSecurityNr > searchTermSocialSecNr))
+                      .ToList();
+        }
+
+        static List<string> PopulateData()
+        {
+            List<string> res = new List<string>();
+            res = (List<string>)File.ReadLines(@"IPL_2021_data.csv");
+
+
+
+            return res;
+        }
+
+        static void DisplayData(List<DataModel> data)
+        {
+            foreach (var d in data)
+            {
+                Console.Write($"{d.FirstName} {d.LastName}");
+                Console.Write($" born in {d.DayOfBirth.Year}");
+                Console.Write($" with SocSecNr {d.SocialSecurityNr}");
+                Console.WriteLine($" (is female: {d.SexIsFemale})");
+            }
+        }
+    }
+}
---
+++ assignments/assignment1/ConsoleApp3/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+
+    public static List<IPLDesign> list = new List<IPLDesign>();
+    public static void Main()
+    {
+        var players = new ProcessCSV();
+        players.CSVData = new List<KeyValuePair<string, string>> {
+        new KeyValuePair<string, string>("Name", "name"),
+        new KeyValuePair<string, string>("Team", "team"),
+        new KeyValuePair<string, string>("Role", "role"),
+        new KeyValuePair<string, string>("Matches", "matches"),
+        new KeyValuePair<string, string>("Runs", "runs"),
+        new KeyValuePair<string, string>("Average", "average"),
+        new KeyValuePair<string, string>("SR", "strikeRate"),
+        new KeyValuePair<string, string>("Wickets", "wickets")
+        };
+        list = players.Import<IPLDesign>(@"IPL_Data.csv");
+    }
+
+    public class ProcessCSV
+    {
+        public List<KeyValuePair<string, string>> CSVData;
+        public List<T> Import<T>(string data)
+        {
+            List<T> list = new List<T>();
+            List<string> row = System.IO.File.ReadAllLines(data).ToList();
+            var dataValues = row[0].Split(',').ToList().Select((v, i) => new
+            {
+                ColName = v,
+                ColIndex = i
+            });
+
+            return list;
+        }
+    }
+    //1
+    public class IPLDesign
+    {
+
+        public string name { get; set; }
+        public string team { get; set; }
+        public string role { get; set; }
+        public int matches { get; set; }
+        public int runs { get; set; }
+        public double average { get; set; }
+        public double strikeRate { get; set; }
+        public int wickets { get; set; }
+    }
+   
+    public class Players
+    {
+        List<string> players_name = new List<string>();
+
+       
+        //3
+        public List<string> BowlerWithAtLeastFortyWickets(string Team)
+        {
+            List<string> list = new List<string>();
+            var result = (from li in Program.list
+                         where li.name == Team && li.wickets >= 40
+                         select li);
+            foreach (var team in result)
+            {
+                list.Add(team.name);
+            }
+            return list;
+
+        }
+        //4
+        public IEnumerable<string> searchPlayerWithGivenName(string playerName)
+        {
+            IEnumerable<string> result = (IEnumerable<string>)Program.list.Where(pl => pl.name.Contains(playerName));
+            return result;
+        }
+       
+            
+
+        //5
+        public List<string> StarPerformers()
+        {
+            List<string> player = new List<string>();
+            var starBatsman = Program.list.OrderByDescending(x => x.runs).First();
+            var  starBowler= Program.list.OrderByDescending(x => x.wickets).First();
+            player.Add(starBatsman.name);
+            player.Add(starBowler.name);
+            
+            return player;
+        }
+
+        //6
+        public List<string> TopThree()
+        {
+            List<string> player = new List<string>();
+            var batsMan = Program.list.OrderByDescending(pl => pl.runs).Take(3);
+            var bowler = Program.list.OrderByDescending(pl => pl.wickets).Take(3);
+            var allRounders = Program.list.OrderByDescending(pl => pl.runs).ThenBy(pl => pl.wickets).Take(3);
+            foreach (var itr in batsMan)
+            {
+                player.Add(itr.name);
+            }
+            foreach (var itr in bowler)
+            {
+                player.Add(itr.name);
+            }
+            foreach (var itr in allRounders)
+            {
+                player.Add(itr.name);
+            }
+
+            return player;
+
+
+        }
+
+        //7
+       public void twoHypotheticalTeam()
+           {
+            var List = Program.list.GroupBy(pl => new { team = pl.team }).Select(av => new { Average = av.Average(c => c.average), team = av.Key.team });
+            var res = List.OrderByDescending(x => x.Average).Take(2);
+            foreach (var player in res)
+            {
+                Console.WriteLine(player.team + "  " + player.Average * 11);
+            }
+
+
+
+        }
+        //8
+        public void NextGen()
+        {   //we are finding the next gen player on the basis of good average of wickets, runs and matches that player have played 
+
+
+        }
+       
+
+    }
+}
+
+
+
+
+
+
+
+
+
